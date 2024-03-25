3e0147be25e59b91a6aa4581f397a89b6fd604a6
mindula-madhuhansa <mimmindula@gmail.com>
2024-03-25 11:13:47 +0530

    add header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,54 @@
+import { LayoutGrid, SearchIcon, ShoppingBag } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex items-center justify-between p-6 shadow-sm">
+      <div className="flex items-center gap-x-8">
+        <Link href="/">
+          <Image src="/logo.png" alt="Logo" width={150} height={100} />
+        </Link>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild className="outline-none">
+            <div className="hidden md:flex items-center gap-x-2 border rounded-full p-2 px-10 bg-slate-200">
+              <LayoutGrid className="h-5 w-5" />
+              <p>Category</p>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Browse Category</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <div className="hidden md:flex items-center gap-x-3 border rounded-full p-2 px-5">
+          <SearchIcon className="h-5 w-5" />
+          <input type="text" placeholder="Search" className="outline-none" />
+        </div>
+      </div>
+
+      <div className="flex items-center gap-x-6">
+        <h3 className="flex items-center gap-x-2 text-lg">
+          <ShoppingBag className="h-5 w-5" /> 0
+        </h3>
+        <Button>Log In</Button>
+      </div>
+    </div>
+  );
 };
